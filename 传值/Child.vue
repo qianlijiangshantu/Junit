e4baff16a152dbf2d24365c6c49af7999d8d4696
wang <ning.123abc@gmail.com>
2022-06-29 14:14:18 +0900

    传值

---
+++ 传值/Child.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="content">
+    <p>単価: {{ tanka }}円</p>
+    <p>数量:<input type="text" class="input" v-model="num" /> 個</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tanka: 100,
+      num: "",
+    };
+  },
+};
+</script>
+<style scoped>
+.content {
+  margin: 20px;
+  background-color: gainsboro;
+  padding: 10px;
+}
+.input {
+  margin-left: 10px;
+  height: 38px;
+  width: 100px;
+  font-size: 30px;
+}
+
+p {
+  font-size: 30px;
+}
+</style>
---
+++ 传值/Kingaku.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>This is a ref sample</h1>
+    <Child ref="refSumPrice" />
+    <button class="btn" @click="cal">計算</button>
+    <hr />
+    <p>合計金額: {{ sumPrice }} 円</p>
+  </div>
+</template>
+
+<script>
+import Child from "./Child.vue";
+export default {
+  data() {
+    return {
+      sumPrice: 0,
+    };
+  },
+  components: {
+    Child,
+  },
+  methods: {
+    cal() {
+      this.sumPrice = this.$refs.refSumPrice.num * this.$refs.refSumPrice.tanka;
+    },
+  },
+  mounted: function () {},
+};
+</script>
+<style>
+p {
+  font-size: 40px;
+}
+.btn {
+  height: 40px;
+  width: 100px;
+  margin-left: 20px;
+  font-size: 20px;
+  background-color: rgba(255, 217, 113, 0.76);
+  border: 2px solid #ccc;
+}
+.btn:hover{
+  cursor: pointer;
+  background-color:yellow;
+}
+</style>
